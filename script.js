cd716796e741bd9ec71da589890a370d40acd7d3
Muhammad Arif <arif1951995@gmail.com>
2019-07-18 17:45:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+ let message = document.querySelector('h1');
+ function emulator(name) {
+  
+    let randomNum = Math.floor(Math.random() * 6);
+    
+    
+
+     if(randomNum === 0) {
+       message.innerHTML =  `${name} you are einstine`
+      
+    }else if(randomNum === 1) {
+        message.innerHTML =  `${name} you are very smart`
+
+    }else if(randomNum === 2) {
+        message.innerHTML =  `${name} you are very intelligence is quit ok`
+
+    }else if(randomNum === 3) {
+        message.innerHTML =  `${name} you are dumb`
+      
+
+    }else if(randomNum === 4) {
+        message.innerHTML =  `${name} you are very  very dumb`
+      
+
+    }else if(randomNum === 5) {
+        message.innerHTML =  `${name} you are very smart`
+       
+
+    }
+ }
+
+ document.querySelector('button').addEventListener('click', function() {
+    
+    emulator(document.querySelector('input').value);
+
+    document.querySelector('input').value = '';
+ })
+
+
+ addEventListener('click', function() {
+     
+ })
