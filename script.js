27d566609a3b52d39bebd3b493e11c50099de0c3
Muhammad Arif <arif1951995@gmail.com>
2019-07-19 00:39:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- let message = document.querySelector('h1');
+let message = document.querySelector('h1');
 let messageArr = [
     `you are einstine`,
     `you are very smart`,
@@ -7,21 +7,12 @@ let messageArr = [
     `you are very dumb`,
     `you are really smart`
 ]
- function emulator(name) {
-  
+function emulator(name) {
     let randomNum = Math.floor(Math.random() * 6);
-    
     message.innerHTML = `Dear ${name} ${messageArr[randomNum]}`;
+}
 
-    
- }
-
- document.querySelector('button').addEventListener('click', function() {
-    
+document.querySelector('button').addEventListener('click', function () {
     emulator(document.querySelector('input').value);
-
     document.querySelector('input').value = '';
- })
-
-
- 
+})
